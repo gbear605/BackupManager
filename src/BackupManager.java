4fa9b1295e4164d6eb091ff7add114465ff1c1fb
LiruJ <liruotaku@googlemail.com>
2015-07-30 22:54:18 +0100

    window

--- src/BackupManager.java
+++ src/BackupManager.java
@@ -1,7 +1,32 @@
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
+
+import java.awt.Container;
+import java.awt.Insets;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class BackupManager extends Component {
 
