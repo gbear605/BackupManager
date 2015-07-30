83a83b37cd2d56021e7e981547f013410e5b292e
gbear605 <garrisontaylor@gmail.com>
2015-07-30 15:56:36 -0400

    Make methods in Backup static

--- src/Backup.java
+++ src/Backup.java
@@ -2,24 +2,24 @@ import java.io.*;
 
 public class Backup {
 	
-	public void backupFiles(BackupFile[] filesToBackup)
+	public static void backupFiles(BackupFile[] filesToBackup)
 	{
 		for(BackupFile fileToBackup : filesToBackup) {
 			backupFile(fileToBackup);
 		}
 	}
 	
-	public void backupFile(BackupFile fileToBackup){
+	public static void backupFile(BackupFile fileToBackup){
 		//TODO: Actually implement backing up
 	}
 	
-	public void restoreFiles(BackupFile[] filesToRestore) {
+	public static void restoreFiles(BackupFile[] filesToRestore) {
 		for(BackupFile fileToRestore : filesToRestore) {
 			restoreFile(fileToRestore);
 		}
 	}
 	
-	public void restoreFile(BackupFile fileToRestore) {
+	public static void restoreFile(BackupFile fileToRestore) {
 		//TODO: Actually implement restoration
 	}
 
